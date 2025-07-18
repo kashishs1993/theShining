92065c0705c1c6ff208aa3b8a897f1a258fde3a8
kashishs1993 <kashishs1993@gmail.com>
2025-07-18 11:53:58 +0200

    added files

---
+++ main.js
@@ -0,0 +1,48 @@
+import * as THREE from 'three';
+import { MindARThree } from 'mindar-image-three';
+import {loadAudio, loadGLTF, loadVideo} from "./libs/loader.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+  const start = async() => {
+    // initialize MindAR 
+    const mindarThree = new MindARThree({
+      container: document.body,
+      imageTargetSrc: './shining.mind',
+       uiScanning: "no",
+      uiLoading: "no",
+    });
+    const {renderer, scene, camera} = mindarThree;
+
+    //video
+    const video = await loadVideo("./Shining2.mp4");
+    const texture = new THREE.VideoTexture(video);
+    
+    const geometry = new THREE.PlaneGeometry(1.5,2);
+    const material = new THREE.MeshBasicMaterial({map: texture});
+    const plane = new THREE.Mesh(geometry, material);
+
+    // create AR object
+    
+    // create anchor
+    const anchor = mindarThree.addAnchor(0);
+    anchor.group.add(plane);
+
+    anchor.onTargetFound = () => {
+      video.loop = true;
+      video.play();
+    }
+    anchor.onTargetLost = () => {
+      video.pause();
+    }
+    video.addEventListener( 'play', (loop) => {
+    });
+
+
+    // start AR
+    await mindarThree.start();
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera);
+    });
+  }
+  start();
+});
